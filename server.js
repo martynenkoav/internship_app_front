fc430bb3fe23c4c110eb02cb30dee01bfbe88843
Nastya <martynenkoav.18@mail.ru>
2022-05-18 15:11:36 +0300

    changed server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/internship-app-front'));
+app.use(express.static('./dist/internship_app_front'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('/index.html', { root: __dirname }),
+  res.sendFile('index.html', {root: 'dist/internship_app_front/'}),
 );
 
 // Start the app by listening on the default Heroku port
