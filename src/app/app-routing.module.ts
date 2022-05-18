2bb4457b6e18bdc9e6f916256a0bdfcfaa5f1c4c
Nastya <martynenkoav.18@mail.ru>
2022-05-18 22:04:39 +0300

    added files for normal view of pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {InternshipComponent} from "./component/internship/internship.component";
+import {CompanyComponent} from "./component/company/company.component";
+import {RegisterComponent} from "./component/register/register.component";
+import {LoginComponent} from "./component/login/login.component";
+import {InternshipAdd} from "./component/internship-add/internship-add";
+import {StudentComponent} from "./component/student/student.component";
+import {CompanyForCheckComponent} from "./component/company-for-check/company-for-check.component";
+import {ErrorPageComponent} from "./component/error-page/error-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InternshipComponent},
+  {path: 'company', component: CompanyComponent},
+  {path: 'internship', component: InternshipComponent},
+  /* {path: 'internship', loadChildren:()=>import('./component/internship/internship.module').then(mod=>mod.InternshipModule)},*/
+  {path: 'register', component: RegisterComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'internship-add', component: InternshipAdd},
+  {path: 'student', component: StudentComponent},
+  {path: 'company-for-check/:id', component: CompanyForCheckComponent},
+  {path: 'error', component: ErrorPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {CompanyComponent} from './component/company/company.component';
+import {HttpClientModule} from "@angular/common/http";
+import {InternshipComponent} from './component/internship/internship.component';
+import {NavComponent} from './component/nav/nav.component';
+import {RegisterComponent} from './component/register/register.component';
+import {LoginComponent} from './component/login/login.component';
+import {MatSelectModule} from "@angular/material/select";
+import {authInterceptorProviders} from "./interceptor/auth.interceptor";
+import {InternshipAdd} from './component/internship-add/internship-add';
+import {StudentComponent} from './component/student/student.component';
+import {CompanyForCheckComponent} from './component/company-for-check/company-for-check.component';
+import {ErrorPageComponent} from './component/error-page/error-page.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ScrollingModule} from "@angular/cdk/scrolling";
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CompanyComponent,
+    InternshipComponent,
+    NavComponent,
+    RegisterComponent,
+    LoginComponent,
+    InternshipAdd,
+    StudentComponent,
+    CompanyForCheckComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    BrowserAnimationsModule,
+    ScrollingModule
   ],
-  providers: [],
+  providers: [
+    authInterceptorProviders
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
